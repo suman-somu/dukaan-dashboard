f1552d25b28983755b70ff21a9bf1e7e6b466525
Suman Sahoo <ssamsomu@gmail.com>
2024-01-09 15:09:54 +0530

    search bar fixes

--- app/dashboard/components/DashboardRight/SearchBar.tsx
+++ app/dashboard/components/DashboardRight/SearchBar.tsx
@@ -9,7 +9,7 @@ interface SearchBarProps {
 
 export const SearchBar = ({ text, fill, width }: SearchBarProps) => {
     return (
-        <div className={`h-fit w-fit flex items-center justify-start border-[1px] border-[${colors.darkgray}] text-[${colors.darkgray}] ${fill ? `bg-[${colors.lightgray}]` : ''}  py-1 px-2 rounded-md`}>
+        <div className={`h-fit w-fit flex items-center justify-star text-[${colors.darkgray}] ${fill ? `bg-[${colors.lightgray}]` : `border-[1px] border-[${colors.darkgray}]`}  py-1 px-2 rounded-md`}>
             <Search className=" mx-1" />
             {text && <p className=" text-xs mx-1 mr-5">{text}</p>}
         </div>
