9e8058aa1125feff9dac7f1d0e3c1d2149761b60
Suman Sahoo <ssamsomu@gmail.com>
2024-01-09 18:41:37 +0530

    brand image

--- app/dashboard/components/DashboardLeft/LeftProfile.tsx
+++ app/dashboard/components/DashboardLeft/LeftProfile.tsx
@@ -6,7 +6,7 @@ export default function LeftProfile() {
       <div className="w-full flex items-center justify-between rounded-md">
         <div className="flex items-center justify-between">
           <Avatar
-            src="https://picsum.photos/seed/picsum/200/300"
+            src="https://media.licdn.com/dms/image/C4E03AQH6v6AuzZw2-A/profile-displayphoto-shrink_800_800/0/1594492975979?e=2147483647&v=beta&t=EA6qKhSypdzFFBJW8cIp5-bYEttJwH1-Y_3ugP78W40"
             fallback="A"
           />
           <div className="h-fit grow mx-2 flex flex-col justify-center">
